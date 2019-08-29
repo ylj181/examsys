042eb880c61dfb16282dc50d8132d7161ff97daf
song <song@aliyun.com>
2019-08-29 19:45:38 +0800

    dao的pojo

--- src/main/java/com/qfedu/examsys/dao/UserDao.java
+++ src/main/java/com/qfedu/examsys/dao/UserDao.java
@@ -3,9 +3,12 @@ package com.qfedu.examsys.dao;
 
 import com.qfedu.examsys.pojo.User;
 
+import java.util.List;
+
 public interface UserDao {
     int deleteByPrimaryKey(Integer id);
 
+    //注册
     int insert(User record);
 
     int insertSelective(User record);
@@ -15,4 +18,12 @@ public interface UserDao {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    //登录
+    User login(String username);
+
+    //根据角色Id查询
+    List<User> findAllRole(Integer rid);
+
+
 }
